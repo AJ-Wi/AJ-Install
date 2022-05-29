508a82abf4cb917b402e77bb8c46240fd3aead0b
ajwipo <tropaguararia28@gmail.com>
2022-05-29 05:13:14 -0500

    initial files

---
+++ main.js
@@ -0,0 +1,16 @@
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
